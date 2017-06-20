7efe296c5480f4f3d07b1afb5c31bb06a736f89a
balau123 <thai.itplus@gmail.com>
2017-06-20 10:27:34 -0400

    1

--- upshell_git.sh
+++ upshell_git.sh
@@ -52,7 +52,7 @@ git remote remove origin
 git reset --mixed
 
 git remote add origin "$path"
-git remote set-url origin git@github.com/TCU1/ChatClientServer_Python
+git remote set-url origin "git@github.com/TCU1/ChatClientServer_Python/"
 git branch NguyenDinhThai
 git checkout NguyenDinhThai
 #git pull origin master
