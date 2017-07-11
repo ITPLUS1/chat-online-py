37e6886be3871dfccae9045aead08a5300686d47
balau123 <thai.itplus@gmail.com>
2017-07-11 16:20:38 +0700

    UP

--- BLL/Process.py
+++ BLL/Process.py
@@ -1,6 +1,9 @@
 #process
 # -*- coding: utf-8 -*-
 import time
+import socket
+#get URL
+import urllib
 
 class Process:
     def __init__(self):
@@ -15,30 +18,170 @@ class Process:
         except:
             return None
 
-    def ServerMsg(self, Mode, PORT):
+    # [Thong bao] Khoi tao server
+    def msgServerBind(self, PORT):
+        MessageContent = "\n\n"+bcolors.BOLD+bcolors.WARNING+"[ " + self.getCurrentDateTime('t') + "]- Khởi Tạo Server | CỔNG " +str(PORT)+bcolors.ENDC+"\n"
+	print MessageContent
+
+
+    def getPublicIP(self):
+        try:
+            ip = urllib.urlopen('http://icanhazip.com/').read().strip('\n')
+        except:
+            try:
+                ipraw = urllib.urlopen('http://whatismyip.org').read().split("</span>")[1]
+                ip2=ipraw.split(">")[-1]
+            except:
+                return None
+                
+            else:
+                #print "ip2",ip2
+                return ip2
+        else:
+            #print "ip1",ip
+            return ip
+        return None
+
+    def getLocalIP(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('google.com', 0))
+            ip=s.getsockname()[0]
+            return ip
+        except:
+            return None
+
+
+    def Line(self):
+        print """
+
+            đã sẵn sàng kết nối
+            
+
+        """
+        print "-"*120
+
+    # [Thong bao] Tinh trang server
+    def msgServerOnline(self):
+        MessageWait = bcolors.BOLD+bcolors.WARNING+"[ " + self.getCurrentDateTime('t') + "]- Đang lấy IP..."+bcolors.ENDC
+        print MessageWait
+        
+        publicIP=self.getPublicIP()
+        localIP=self.getLocalIP()
+
+
+        Status="Offline!!!"
+        if(publicIP != None):
+            Status="Online"
+        else:
+            publicIP=""
+
+
+        if(localIP == None):
+            localIP=""
+
+    
+        MessageContent = bcolors.BOLD+bcolors.WARNING+"[ " + self.getCurrentDateTime('t') + "]- Server "+Status+bcolors.ENDC+"\n"
+        MessageContent2 = " Public IP: "+publicIP+"\n"+" LOcal IP: "+localIP+"\n"
+        
+	print MessageContent
+	print MessageContent2
+
+
+
+    # [Thong bao] Server ban
+    def msgServerBusy(self, PORT):
+        MessageContent = bcolors.BOLD+bcolors.FAIL+"[ " + self.getCurrentDateTime('t') + "]- SERVER ĐANG BẬN, THỬ CỔNG " +str(PORT)+bcolors.ENDC+"\n"
+	print MessageContent
+
+    # [Thong bao] Server Loi
+    def msgServerError(self, PORT):
+        MessageContent = bcolors.BOLD+bcolors.FAIL+"[ " + self.getCurrentDateTime('t') + "]- KHÔNG THỂ TẠO SERVER " +str(PORT)+bcolors.ENDC+"\n"
+	print MessageContent
+
+
+    # [Thong bao] Thong tin Server 
+    def ServerMsg(self, Mode):
         MessageContent = ""
-        NewTime = self.getCurrentDateTime('d')
+        NewTime = self.getCurrentDateTime('t')
 
         if (Mode == 'on'):
-            MessageContent = bcolors.BOLD+"""
-            """ + NewTime + """
-            Server Đang mở, Port """ + str(PORT) + """
-
-
-            """
+            MessageContent = bcolors.BOLD+bcolors.WARNING+"[ " + self.getCurrentDateTime('t') +"]- Server Đã mở" +bcolors.ENDC+"\n"
 
         elif (Mode == 'off'):
-            MessageContent = bcolors.BOLD+bcolors.FAIL+"""
-
-            """ + NewTime + """
-            Server Đã đóng, Port """ + str(PORT) + """
-
-
-            """
-        return MessageContent
+            MessageContent = bcolors.BOLD+bcolors.FAIL+"[ " + self.getCurrentDateTime('t') + "]- Server Đã đóng " +bcolors.ENDC+"\n"
+        print MessageContent
 
     def show(self, MessageContent):
         print MessageContent
+
+
+    #Xoa user ra khoi danh sach
+    def RemoveNickName(self, NickName,sock):
+        NickNameList=NickName
+        User=NickNameList[sock.getpeername()]
+        for key,value in NickNameList.items():
+            if value==User:
+                NickNameList.pop(key)
+            
+        return NickNameList        
+
+
+
+    # [Thong bao] dem so thanh vien hien co
+    def MsgCountUser(self,NickName):
+        ListUser=[]
+        Count=0
+        for key,value in NickName.items():
+            Count+=1
+            ListUser.append(value)
+            
+        ListUser=",".join(ListUser)
+            
+        MessageContent= bcolors.BOLD+bcolors.OKBLUE + "[ " + self.getCurrentDateTime('t') + " ]"+bcolors.ENDC+bcolors.OKGREEN+"- Thành viên đang online: "+str(Count)+"\n\t\t"+ListUser+bcolors.ENDC+"\n"
+        return MessageContent
+
+    # [Thong bao] den new user
+    def MsgNewUser(self,sock,NewNickName,Server):
+        MessageContent=[]
+        msgShow= bcolors.BOLD+bcolors.OKBLUE + "[ " + self.getCurrentDateTime('t') + " ]"+bcolors.ENDC+"-[ New Socket ] "+str(sock.getpeername())+"\n"
+        Message =bcolors.BOLD+bcolors.OKBLUE + "[ " + self.getCurrentDateTime('t') + " ]"+bcolors.ENDC+bcolors.OKGREEN+"- [ SERVER "+Server+" ]*****Chào mừng thành viên: "+NewNickName+" đã vào phòng chat"+bcolors.ENDC+"\n"
+        MessageContent.append(msgShow+ Message)
+        MessageContent.append(Message)
+        return MessageContent
+
+
+    # [Thong bao] trung ten new user
+    def MsgUserExist(self,sock,NewNickName):
+        MessageContent=[]
+        Message = "[ " + self.getCurrentDateTime('t') + " ]- #Tên : "+NewNickName+" đã tồn tại, vui lòng nhập tên khác"
+        MessageContent.append(str(sock.getpeername()) + Message)
+        MessageContent.append(Message)
+        return MessageContent
+
+
+
+    # [Thong bao] den user [Logout] huac mat ket noi
+    def MsgUserLogout(self, NickName,sock):
+        MessageContent = []
+        Message = bcolors.BOLD+bcolors.OKBLUE+"[ " + self.getCurrentDateTime('t') + " ]"+bcolors.ENDC+bcolors.FAIL+"- ***** Thành viên: "+NickName[sock.getpeername()] + " Đã thoát!!!"+bcolors.ENDC+"\n"
+        MessageContent.append(NickName[sock.getpeername()] + Message)
+        MessageContent.append(Message)
+        return MessageContent
+
+    # [Thong bao] Message [Nhan duoc] tu user
+    def ProcessRecvData(self,NickName,sock,RecvData):
+        MessageContent = bcolors.BOLD+bcolors.OKBLUE+"[ " + self.getCurrentDateTime('t') + " ]"+bcolors.ENDC+bcolors.White+"- "+ NickName[sock.getpeername()] + " - " + RecvData.strip().replace("\r\n","")
+        #print "RecvData - MessageContent ",MessageContent
+        return MessageContent
+
+
+
+
+
+
+
+
 
 
 
@@ -58,22 +201,11 @@ class bcolors:
 
 
 
-
-
-
-
-
-
 class Msg:
     def __init__(self):
         P = Process()
         self.msg1= "[ " + P.getCurrentDateTime('t') + "]- [AUTO] [ADMIN*THAI] : Chào mừng đến với phòng chat :)"
         self.msg2= """
-        Đêm nay thật buồn, vì anh biết em đang ở nơi rất xa 
-        Một nơi mà anh không thể đến được bên em ngay trong lúc này 
-        Nhưng dù cho đó là nơi nào, một khi con tim chúng ta còn thuộc về nhau 
-        Anh không cảm thấy cô đơn nhưng rất buồn vì nhớ em. 
-    
         Đường phố thinh lặng, 
         Chỉ còn cơn gió thoáng đưa một chút sương lạnh 
         Làm buốt vai anh, chợt thấy đêm dài, 
